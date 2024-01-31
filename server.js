9b21c926f5e2022501e9951f5f9b9f31bf38eaee
mayur2004Id <gupta.mayur@kalvium.community>
2024-01-31 16:24:25 +0530

    complite routes

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 const data = 'mongodb+srv://mayur:mayurgupta2004@cluster0.qvdmxxy.mongodb.net/?retryWrites=true&w=majority'
+const routes= require("./routes")
+
+app.use('/',routes)
 
 app.get('/',(req,res)=>{
   mongoose.connect(data)
