29b32176bae12b05ad2c5a7722577438d7b92763
mayur2004Id <gupta.mayur@kalvium.community>
2024-02-01 11:04:33 +0530

    review

---
+++ frentend_part/my_react_app/src/component/SignUpForm.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import './SignUpForm.css';
+
+const SignUpForm = () => {
+  
+  // State for user information
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState(''); 
+  const [mobileNumber, setMobileNumber] = useState('');
+
+  // Function to handle form submission
+  const handleSignUp = () => {
+    console.log('User Details:', { firstName, lastName, email, mobileNumber });
+   
+  };
+
+  return (
+    <div  id="boder">
+      <h2>Sign Up</h2>
+      <form>
+        <label>
+          First Name:
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </label>
+        <br />
+
+        <label>
+          Last Name:
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </label>
+        <br />
+
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+
+        <label>
+          Mobile Number:
+          <input
+            type="text"
+            value={mobileNumber}
+            onChange={(e) => setMobileNumber(e.target.value)}
+          />
+        </label>
+        <br />
+
+        <button type="button" onClick={handleSignUp}>
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
+
